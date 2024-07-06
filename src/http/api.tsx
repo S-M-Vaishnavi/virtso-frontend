dbff85e6c0b3699ef1d460cab5abe9875588dce7
S-M-Vaishnavi <smvaishnavi23@gmail.com>
2024-07-06 15:07:42 +0530

    Have to work on authentication of dashboard

---
+++ src/http/api.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Api = () => {
+const url = "http://localhost:8000"
+
+  return (
+    
+    <div>
+      
+    </div>
+  )
+}
+
+export default Api
---
+++ src/pages/Authentication/RecoverIdBox.tsx
@@ -0,0 +1,82 @@
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { IRootState } from '../../store';
+import { useEffect, useState } from 'react';
+import { setPageTitle, toggleRTL } from '../../store/themeConfigSlice';
+import IconMail from '../../components/Icon/IconMail';
+
+const RecoverIdBox = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(setPageTitle('Recover Id Box'));
+    });
+    const navigate = useNavigate();
+    const isRtl = useSelector((state: IRootState) => state.themeConfig.rtlClass) === 'rtl' ? true : false;
+    const themeConfig = useSelector((state: IRootState) => state.themeConfig);
+    const setLocale = (flag: string) => {
+        setFlag(flag);
+        if (flag.toLowerCase() === 'ae') {
+            dispatch(toggleRTL('rtl'));
+        } else {
+            dispatch(toggleRTL('ltr'));
+        }
+    };
+    const [flag, setFlag] = useState(themeConfig.locale);
+
+    const submitForm = () => {
+        navigate('/');
+    };
+
+    return (
+        <div>
+            <div className="absolute inset-0">
+                <img src="/assets/images/auth/bg-gradient.png" alt="image" className="h-full w-full object-cover" />
+            </div>
+
+            <div className="relative flex min-h-screen items-center justify-center bg-[url(/assets/images/auth/map.png)] bg-cover bg-center bg-no-repeat px-6 py-10 dark:bg-[#060818] sm:px-16">
+                <img src="/assets/images/auth/coming-soon-object1.png" alt="image" className="absolute left-0 top-1/2 h-full max-h-[893px] -translate-y-1/2" />
+                <img src="/assets/images/auth/coming-soon-object2.png" alt="image" className="absolute left-24 top-0 h-40 md:left-[30%]" />
+                <img src="/assets/images/auth/coming-soon-object3.png" alt="image" className="absolute right-0 top-0 h-[300px]" />
+                <img src="/assets/images/auth/polygon-object.svg" alt="image" className="absolute bottom-0 end-[28%]" />
+                <div className="relative w-full max-w-[870px] rounded-md bg-[linear-gradient(45deg,#fff9f9_0%,rgba(255,255,255,0)_25%,rgba(255,255,255,0)_75%,_#fff9f9_100%)] p-2 dark:bg-[linear-gradient(52.22deg,#0E1726_0%,rgba(14,23,38,0)_18.66%,rgba(14,23,38,0)_51.04%,rgba(14,23,38,0)_80.07%,#0E1726_100%)]">
+                    <div className="relative flex flex-col justify-center rounded-md bg-white/60 backdrop-blur-lg dark:bg-black/50 px-6 lg:min-h-[758px] py-20">
+                        <div className="mx-auto w-full max-w-[440px]">
+                            <div className="mb-7">
+                                <h1 className="mb-3 text-2xl font-bold !leading-snug dark:text-white">Password Reset</h1>
+                                <p>Enter your email to recover your ID</p>
+                            </div>
+                            <form className="space-y-5" onSubmit={submitForm}>
+                                <div>
+                                    <label htmlFor="Email" className="dark:text-white">
+                                        Email
+                                    </label>
+                                    <div className="relative text-white-dark">
+                                        <input id="Email" type="email" placeholder="Enter Email" className="form-input ps-10 placeholder:text-white-dark" />
+                                        <span className="absolute start-4 top-1/2 -translate-y-1/2">
+                                            <IconMail fill={true} />
+                                        </span>
+                                    </div>
+                                </div>
+                                <button type="submit" className="btn btn-gradient !mt-6 w-full border-0 uppercase shadow-[0_10px_20px_-10px_rgba(67,97,238,0.44)]">
+                                    SEND EMAIL
+                                </button>
+                            </form>
+                            <div className="relative my-7 text-center md:mb-9">
+                                <span className="absolute inset-x-0 top-1/2 h-px w-full -translate-y-1/2 bg-white-light dark:bg-white-dark"></span>
+                                <span className="relative bg-white px-2 font-bold uppercase text-white-dark dark:bg-dark dark:text-white-light">or</span>
+                            </div>
+                            <div>
+                                    <label className="flex cursor-pointer items-center justify-center gap-5">
+                                        <span className="text-white-dark">GO TO</span>
+                                        <Link to="/" className='underline'>LOG IN</Link>
+                                    </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default RecoverIdBox;
